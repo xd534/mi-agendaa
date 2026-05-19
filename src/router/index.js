9b3dd3b3b131fbe4a39611e135a14fdd8772f3e2
xd534 <ruizrafaelsamuel1f@gmail.com>
2026-05-18 18:49:16 -0600

    primer commi

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
 
 const routes = [
